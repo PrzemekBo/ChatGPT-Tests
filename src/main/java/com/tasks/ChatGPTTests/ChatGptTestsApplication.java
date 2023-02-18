dfb8dbc2e938d31b48dfde51104ba785dfe91e4f
Przemek <przemek@macbook-pro-przemek.home>
2023-02-18 17:58:45 +0100

    Initial commit

---
+++ src/main/java/com/tasks/ChatGPTTests/ChatGptTestsApplication.java
@@ -0,0 +1,13 @@
+package com.tasks.ChatGPTTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatGptTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasks/ChatGPTTests/ChatGptTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasks.ChatGPTTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
