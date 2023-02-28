f0ac4c6692e7b9b4f0912c859c4a26131d74b3d4
przemek <przemek.bocer@gmail.com>
2023-02-28 19:13:35 +0100

    ByteToFileConverter

---
+++ src/main/java/com/tasks/ChatGPTTests/NewClass.java
@@ -0,0 +1,38 @@
+package com.tasks.ChatGPTTests;
+
+public class NewClass {
+
+
+    import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Iterator;
+
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
+import org.apache.poi.xwpf.usermodel.XWPFStyles;
+
+    public class WordToCSSConverter {
+
+        public static String extractCSSFromWordDocument(byte[] documentBytes) throws IOException {
+            InputStream inputStream = new ByteArrayInputStream(documentBytes);
+            XWPFDocument document = new XWPFDocument(inputStream);
+            XWPFStyles styles = document.getStyles();
+            StringBuilder cssBuilder = new StringBuilder();
+            Iterator<XWPFParagraph> paragraphIterator = document.getParagraphsIterator();
+            while (paragraphIterator.hasNext()) {
+                XWPFParagraph paragraph = paragraphIterator.next();
+                String styleId = paragraph.getStyleID();
+                if (styleId != null) {
+                    String styleName = styles.getStyle(styleId).getName();
+                    if (styleName != null) {
+                        String styleCSS = styles.getStyle(styleId).getCTStyle().getPPr().toString();
+                        cssBuilder.append(styleCSS);
+                    }
+                }
+            }
+            return cssBuilder.toString();
+        }
+    }
+
+}
